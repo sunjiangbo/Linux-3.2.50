a43806b242d2f12501ec0d75d5a75e1d00529770
pursuitxh <pursuitxh@gmail.com>
2013-09-28 08:29:25 -0400

    Add usb gadget support

--- arch/arm/mach-s3c2440/mach-smdk2440.c
+++ arch/arm/mach-s3c2440/mach-smdk2440.c
@@ -202,6 +202,7 @@ static struct platform_device *smdk2440_devices[] __initdata = {
 	&s3c_device_i2c0,
 	&s3c_device_iis,
 	&smdk2440_device_eth,
+	&s3c_device_usbgadget,
 };
 
 static void __init smdk2440_map_io(void)
